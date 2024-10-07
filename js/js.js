0669aacdfb9d5f525269b341072bcc2e53e47c52
PistonCube <93083195+PistonCube@users.noreply.github.com>
2024-10-07 14:21:29 -0300

    New Website
    1.0

---
+++ js/js.js
@@ -0,0 +1,6 @@
+  /**
+   * Get the current year
+   */
+  const year = new Date().getFullYear();
+  // Insert the year into the element with id "year"
+  document.getElementById('year').textContent = year;
